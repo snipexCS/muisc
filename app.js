5d625f9846318ed01cc240860e2c171385470b2b
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-02-20 16:07:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector('.banner button').addEventListener('click',()=>{
+document.querySelector('.banner').style.display='none'
+document.querySelector('.slideshow-wrapper').style.cssText="opacity: 1;visibility:visible"
+});
